009977b407c0c3f2421d39e13f9d2a70058cda2d
Jihun-Kim <anam4561@naver.com>
2023-06-22 22:07:44 +0900

    Counter UI added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Clicked: times
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
+}
+
+export default App;

c679d4a0e07b5babc3e70bebd2e6dee35446786e
Jihun-Kim <anam4561@naver.com>
2023-07-11 22:38:41 +0900

    add increment

--- src/index.tsx
+++ src/index.tsx
@@ -27,7 +27,7 @@ const render = () =>
         <App
           value={store.getState()}
           onIncrement={() => store.dispatch({ type: "INCREMENT" })}
-          onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+          onDecrement={() => store.dispatch({ type: "DECREMENT" })} //
         />
       </Provider>
     </React.StrictMode>

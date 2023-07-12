9ea874b42494a63e05d892127891e8edcfbb29ce
Jihun-Kim <anam4561@naver.com>
2023-07-12 22:19:31 +0900

    add redux-thunk

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./reducers";
+import { fetchPosts } from "./actions/posts";
 
 type Props = {
   value: any;
@@ -9,17 +10,29 @@ type Props = {
   onDecrement: () => void;
 };
 
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+}
+
 function App({ value, onIncrement, onDecrement }: Props) {
   const dispatch = useDispatch();
   const counter = useSelector((state: RootState) => state.counter);
   const todos: string[] = useSelector((state: RootState) => state.todos);
+  const posts: Post[] = useSelector((state: RootState) => state.posts);
   const [todoValue, setTodoValue] = useState("");
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTodoValue(e.target.value);
   };
   const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    dispatch({type:"ADD_TODO", text: todoValue})
+    dispatch({ type: "ADD_TODO", text: todoValue });
     setTodoValue("");
   };
   return (
@@ -27,15 +40,20 @@ function App({ value, onIncrement, onDecrement }: Props) {
       Clicked: {counter} times
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
-
       <ul>
-        {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
       </ul>
-
       <form onSubmit={addTodo}>
         <input type="text" value={todoValue} onChange={handleChange} />
         <input type="submit" />
       </form>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
